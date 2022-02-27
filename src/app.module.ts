816dc33966cd3a299b28bf3be9a8e2e445399407
dar <gown4887@gmail.com>
2022-02-27 23:17:51 +0900

    클론코딩 스터디

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestrantsModule } from './restrants/restrants.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username:"nubereatsremote",
+      password:"Asd1fgh2",
+      database: "nuber-eats",
+      autoLoadEntities: true,
+      synchronize:true,
+      logging:true
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver:ApolloDriver,
       autoSchemaFile:true
